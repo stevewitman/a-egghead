19a020a51a416c5b798c93a072e8118326115aee
Steve Witman <stevewitman@gmail.com>
2015-02-17 15:12:16 -0700

    directive restrictions (attributes)

---
+++ 012-directive-restrictions/main.js
@@ -0,0 +1,11 @@
+var app = angular.module("superhero", [])
+
+app.directive("superman", function () {
+  return {
+    restrict:"A",
+    link: function () {
+      alert("Im working")
+
+    }
+  }
+})
