07f58a05706f5c325104b08ccfbf3540aa258700
Steve Witman <stevewitman@gmail.com>
2015-02-17 10:04:26 -0700

    filter

---
+++ 005-Filter/main.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('myApp', [])
+.controller("FirstCtrl", function FirstCtrl($scope, Data) {
+  $scope.data = Data;
+})
+.controller("SecondCtrl", function SecondCtrl($scope, Data) {
+  $scope.data = Data;
+  $scope.reversedMessage = function(message) {
+    return message.split("").reverse().join("");
+  }
+})
+myApp.factory('Data', function() {
+  return {message:"I'm data from a service"}
+})
+myApp.filter('reverse', function () {
+  return function (text) {
+    return text.split("").reverse().join("");
+  }
+})
